cb3223ef4aa11b2e2135b3885e5743bfe6f6ebed
Elizabeth <elizabethjqiu@gmail.com>
2023-12-02 23:35:52 -0500

    d2p2

--- src/bin/2.rs
+++ src/bin/2.rs
@@ -5,6 +5,7 @@
 fn main() {
     let input = include_str!("2.txt");
     let mut res = 0;
+    let mut res2 = 0;
     let mut blue = 0;
     let mut red = 0;
     let mut green = 0;
@@ -28,48 +29,32 @@ fn main() {
                 } else {
                     blue = blue.max(val);
                 }
-
-                // println!("val is {}, color is {}", val, color);
-                // println!("Exiting stmt");
             }
         }
 
         if red <= 12 && green <= 13 && blue <= 14 {
-            // println!("adding id {} to res {}", id, res);
             res += id.replace("Game ", "").to_string().parse::<i32>().unwrap(); // replace &str portion, convert to String, convert to int, add to res as a valid game
-            // println!("now res is {}", res);
         }
 
+        let powerset = red * green * blue;
+        println!("adding powerset {} to res2 {}", powerset, res2);
+        res2 += powerset;
+
         // debugging
-        // println!("res is {}, rounds is {:?}", res, rounds);
         blue = 0;
         red = 0;
         green = 0;
     }
 
     println!("d2p1: {}", res);
+    println!("d2p2: {}", res2);
 }
 
 // brute force / initial ideas:
 
 // 🌙 PART 2
-// 0) parsing words....
-// 1) issues like "oneight" -> should be "one" as first, "eight" as last = 18.
-// 2) should i focus on processing text only, or do text and num together while parsing thru?
-// 3) finding first is straightforward, finding last digit will be reversing line, and adding 1 char at a time
-
-//     mapping = {"red": 0, "blue": 0, "green": 0}
-//     for set in sets:
-//         set = set.strip()
-//         if set == "":
-//             continue
-//         balls = set.split(",")
-//         for ball in balls:
-//             num_balls, color = ball.strip().split(" ")
-//             num_balls = int(num_balls)
-//             mapping[color] = max(mapping[color], num_balls)
-
-//     sum_of_games += mapping["red"] * mapping["blue"] * mapping["green"]
+// powerset = multiply all max vals of red, green, blue per line
+// add to total (res2)
 
 // 🌙 PART 1
 // observe each line structure, where n varies.
@@ -85,9 +70,7 @@ fn main() {
 // so much splitting
 // and then like day 1, i presume we go in and try to identify these numbers and compare
 
-// preview an iterator: use .collect::<Vec<_>>() and then print the vector
-// let first_backward = digits.last().copied().unwrap();
-// iterator trait: https://doc.rust-lang.org/std/iter/trait.Iterator.html
-// .last()  returns Option<&i32>
-// .copied() removes the reference inside
+// .replace("strtoreplace", "replacement")
+// .to_string() for &str -> String
+// .parse::<i32>() for getting int from String
 // .unwrap() removes the Option to access the inner i32
